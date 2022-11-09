cd5afb1ec19ef4a14c7caf17815aff630a6238aa
mahalteja <106660291+mahalteja@users.noreply.github.com>
2022-11-09 13:40:27 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double a,b,c;         a=sc.nextFloat();         b=sc.nextFloat();         c=a*b;
            System.out.format("%.2f",c);
            
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double a,b,c;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        c=a*b;
+        
+        System.out.format("%.2f",c);
+        
+    }
+}
