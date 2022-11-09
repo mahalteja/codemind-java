df84b5c12d534845618f28edf53d3a947185b32d
mahalteja <106660291+mahalteja@users.noreply.github.com>
2022-11-09 13:36:20 +0530

    import java.util.*; class pattern35java {     public static void main(String[] args) {     Scanner sc= new Scanner(System.in);     int t,s,b;     t = sc.nextInt();     s = sc.nextInt();     b = sc.nextInt();     System.out.print(2*(t*s*b)*512);     }  }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class pattern35java {
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int t,s,b;
+    t = sc.nextInt();
+    s = sc.nextInt();
+    b = sc.nextInt();
+    System.out.print(2*(t*s*b)*512);
+    }
+ }   
