113372204334230889fc83e194afa74b7a982b74
mahalteja <106660291+mahalteja@users.noreply.github.com>
2022-11-09 13:33:11 +0530

    import java.util.Scanner; class uniqueno { 	public static void main(String args[]) { 		Scanner sc = new Scanner(System.in); 		String s1 = sc.nextLine(); 		System.out.print(s1); 	} }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class uniqueno {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.nextLine();
+		System.out.print(s1);
+	}
+}
